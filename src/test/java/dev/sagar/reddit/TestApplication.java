63348bb9ae7f6b1cc4587bb79acd2391920c2e5f
nathsagar96 <nathsagar96@gmail.com>
2024-10-13 22:24:20 +0530

    initail commit with dependencies and database setup

---
+++ src/test/java/dev/sagar/reddit/TestApplication.java
@@ -0,0 +1,11 @@
+package dev.sagar.reddit;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestContainersConfiguration.class).run(args);
+	}
+
+}
